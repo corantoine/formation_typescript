4402c9860109893315d0bac816ad0ff71db99d39
corantoine <coralie.antoine@maif.fr>
2026-01-19 14:29:39 +0100

    exercice 7

--- 07_destructuration.js
+++ 07_destructuration.js
@@ -1,53 +1,57 @@
-
 /**
  * utilisez la décomposition pour extraire la premiere et la 2e case du tableau
  * retournez les dans un nouveau tableau
- * 
+ *
  * exemple: [1, 2, 3] => [1, 2]
- * 
+ *
  * astuce: vous pouvez utiliser la décomposition directement dans les arguments de la fonction
- * 
- * contrainte: 
+ *
+ * contrainte:
  *  - interdiction d'utiliser [0] et [1]
  *  - interdiction d'utiliser slice ou splice
  */
 
-const extractFirstTwo = null;
+const extractFirstTwo = ([first, second]) => [first, second];
+console.log(extractFirstTwo([1, 2, 3, 4]));
 
 /**
  * utilisez la décomposition pour extraire la premiere case du tableau et le reste du tableau
  * retournez uniquement le reste du tableau
- * 
+ *
  * exemple: [1, 2, 3] => [2, 3]
  */
 
-const extractRest = null;
+// const extractRest = ([first, ...rest]) => [...rest];
+const extractRest = ([_, ...rest]) => [...rest];
+console.log(extractRest([1, 2, 3]));
 
 /**
  * utilisez la décomposition pour extraire le champ "name" de l'objet passé en paramètre
  * retournez le champ "name"
- * 
+ *
  * exemple: {name: "toto", age: 42} => "toto"
- * 
+ *
  * astuce: vous pouvez utiliser la décomposition directement dans les arguments de la fonction
- * 
+ *
  * contrainte:
  * - interdiction d'utiliser l'opérateur "." pour accéder au champ "name"
  */
-
-const extractName = null;
+const extractName= ({name}) => name
+console.log(extractName({ name: "toto", age: 42 }));
 
 /**
  * utilisez la décomposition pour retourner l'objet utilisateur sans le champ "password"
- * 
+ *
  * exemple: {name: "toto", password: "1234"}
- * 
- * contrainte: 
+ *
+ * contrainte:
  *    - interdiction d'utiliser "delete"
  *    - interdiction d'utiliser l'opérateur "." pour accéder au champ "password"
- * 
+ *
  */
 
-const removePassword = null;
+const removePassword = ({password,  ...obj}) => ({...obj})  
+console.log(removePassword({name:"toto", password:"1234"}));
 
-module.exports = {extractFirstTwo, extractRest, extractName, removePassword}
+
+module.exports = { extractFirstTwo, extractRest, extractName, removePassword };
